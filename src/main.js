41826742ea8889f3c258ac76b5b2c43a0cd5ac94
Elisalilith <giulianalzurut@gmail.com>
2022-02-04 12:06:52 -0300

    secciones comentadas y detalles de css de tarjetas

--- src/main.js
+++ src/main.js
@@ -15,33 +15,39 @@ for (let i=0; i<dataAthlete.length; i++){
 
 //------------------------------visualizacion en pagina de data-------------------
 
-window.onload = () => {
+/*window.onload = () => {
   agregarElementos(dataAthlete)
-}
+}*/
 
     let lista=document.getElementById("root");
     dataAthlete.forEach(function(item){
 
     let divCard= document.createElement("div");    
         divCard.classList.add("card");
+
     let divContent=document.createElement("div");
         divContent.classList.add("content");
+
     let divFront= document.createElement("div");
         divFront.classList.add("front");
     let contentFront= document.createElement("p");
-    let contenido = document.createTextNode(item.name);
+    let contenidoFront = document.createTextNode(item.name);
     
     let divBack= document.createElement("div")
         divBack.classList.add("back");
-    let contenidoBack = document.createTextNode("reverso");
+    let contentBack= document.createElement("p");
+    let contenidoBack = document.createTextNode(item.team + " " + item.age+ " " + item.height);
     
         lista.appendChild(divCard);
         divCard.appendChild(divContent);
+        
         divContent.appendChild(divFront);
         divFront.appendChild(contentFront);
-        contentFront.appendChild(contenido);
-        divBack.appendChild(contenidoBack);
+        contentFront.appendChild(contenidoFront);
+
         divContent.appendChild(divBack);
+        divBack.appendChild(contentBack);
+        contentBack.appendChild(contenidoBack);
     })
     
 
@@ -49,22 +55,37 @@ window.onload = () => {
 //--------------lista desplegable select-----------------
 
 // Lista de países en select desplegable
+
+//constante que se va a usar para realizar el filtrado, hace recorrido de data atletas y trae el nombre de pais
 const listaPaisesRepetidos = dataAthlete.map((paises) => paises.team);
+
+//constante que se va a usar en la seleccion de pais ,  hace filtrado de paises por key, posicion y array, donde tomara el indice de cada key dentro del array (solo de pais por la variable anterior)sin repetirse
 const listaPaises = listaPaisesRepetidos.filter((elemento, indice, array) =>
   array.indexOf(elemento) === indice);
+
+//creacion de lista que se va a agregar en el DIV PAISES de html
 const selectPais = document.querySelector('#paises');
 (() => {
+  //ordena la lista de paises de la variable anterior de manera alfabetica
   const paisesOrdenados = listaPaises.sort();
   paisesOrdenados.forEach((pais) => {
+
+   // al ordenar  con el codigo anterior se veran reflejados con la etiqueta option en HTML
     const opcion = document.createElement('option');
     opcion.textContent = pais;
     opcion.setAttribute('value', pais);
+
+    //para visualizar se crea un hijo con la etiqueta opcion que ira dentro de select
     selectPais.appendChild(opcion);
+
   });
 })();
 
 
+
+
 // Lista de deportes en select desplegable
+
 const listaDeportesRepetidos = dataAthlete.map((deportes) => deportes.sport);
 const listaDeportes = listaDeportesRepetidos.filter((elemento, indice, array) =>
   array.indexOf(elemento) === indice);
