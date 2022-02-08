df1739096fa42074f609f76fe3b2567c4e22fd03
Elisalilith <giulianalzurut@gmail.com>
2022-02-08 09:41:41 -0300

    funcionamiento barras desplegables y se añade RB

--- src/data.js
+++ src/data.js
@@ -7,3 +7,24 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+
+//funcion para filtrar por pais desde nacionalidad
+export const filtroPaises = (data, valor) =>{
+  const tomarPais = data.filter(atleta =>(atleta.team===valor));
+  return tomarPais;
+};
+
+
+//funcion para filtrar por pais desde nacionalidad
+export const filtroDeportes = (data, valor) =>{
+  const tomarDeporte = data.filter(atleta =>(atleta.sport===valor));
+  return tomarDeporte;
+};
+
+
+//funcion para filtrar por pais desde nacionalidad
+export const filtroMedallas = (data, valor) =>{
+  const tomarMedalla = data.filter(atleta =>(atleta.medal===valor));
+  return tomarMedalla;
+};
+
