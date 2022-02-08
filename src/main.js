1f54ca75168ba98e5376a220223f319a0fe5a9f8
GabrielaRomeroMolina <gaba.rome@gmail.com>
2022-02-08 04:42:27 -0300

    camvios en las tarjetas

--- src/main.js
+++ src/main.js
@@ -38,7 +38,7 @@ for (let i=0; i<dataAthlete.length; i++){
     let divBack= document.createElement("div")
         divBack.classList.add("back");
     let contentBack= document.createElement("p");
-    let contenidoBack = document.createTextNode(item.team + " " + item.age+ " " + item.height);
+    let contenidoBack = document.createTextNode(`Pais : ${item.team} Edad : ${item.age} Estatura : ${item.height} Peso : ${item.weight} Deporte : ${item.sport} `);
     
         lista.appendChild(divCard);
         divCard.appendChild(divContent);
