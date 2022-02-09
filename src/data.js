fb483fd0862978959377497681fd3589b1c7debc
Elisalilith <giulianalzurut@gmail.com>
2022-02-09 08:23:54 -0300

    se agrega desplegable genero y se conectan las barras en funcion

--- src/data.js
+++ src/data.js
@@ -15,16 +15,22 @@ export const filtroPaises = (data, valor) =>{
 };
 
 
-//funcion para filtrar por pais desde nacionalidad
+//funcion para filtrar por deporte
 export const filtroDeportes = (data, valor) =>{
   const tomarDeporte = data.filter(atleta =>(atleta.sport===valor));
   return tomarDeporte;
 };
 
 
-//funcion para filtrar por pais desde nacionalidad
+//funcion para filtrar por medallas
 export const filtroMedallas = (data, valor) =>{
   const tomarMedalla = data.filter(atleta =>(atleta.medal===valor));
   return tomarMedalla;
+};
+
+//funcion para filtrar por genero
+export const filtroGenero = (data, valor) =>{
+  const tomarGenero = data.filter(atleta =>(atleta.gender===valor));
+  return tomarGenero;
 };
 
